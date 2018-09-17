a5d6d9114c2cc4d5157aec2f1d7fec0d7403e8a7
kawashima41 <i.kawashima41@gmail.com>
2018-09-17 23:33:41 +0900

    create contract & web view

--- src/Index.vue
+++ src/Index.vue
@@ -1,15 +1,108 @@
 <template lang="pug">
-  h1 Hello, world!!
+div
+  h1 {{ title }}
+  h2 総発行量: {{ totalSupply }} {{ symbol }}
+  h2 あなたの残高: {{ balance }} {{ symbol }}
+
+  h2 By Tokens 
+  form(v-on:submit.prevent="onBuyToken")
+    input(type="text" ref="buyAmount")
+    input(type="submit" value="購入")
+
+  h2 Transfer Tokens 
+  form(v-on:submit.prevent="onTransferToken")
+    input(type="text" ref="transferTo")
+    input(type="text" ref="transferValue")
+    input(type="submit" value = "送金")
+  h2 Transaction List
+  
+  ol
+    li(v-for="tx in transactions")
+        a(href="#") {{ tx }}
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
+import Web3 from 'web3';
+import ContractInfo from './contractInfo';
+import contractInfo from './contractInfo';
+import { Contract } from 'web3/types';
+import { setInterval } from 'timers';
+
+let web3: Web3;
+let token: Contract;
+let address: string;
+
+const gasLimit = 3000000;
+const gasPrice = 10000000000;
+
 @Component
 export default class Index extends Vue {
+  title = 'kawaCoin Exchanged';
+  totalSupply = '0';
+  symbol = 'XXX';
+  balance = '0.0';
+  transactions: string[] = [];
+
+  onBuyToken() {
+    const amount = this.$refs.buyAmount as HTMLInputElement;
+    token.methods
+      .buyToken()
+      .send({
+        from: address,
+        value: amount.value,
+        gas: gasLimit,
+        gasPrice: gasPrice,
+      })
+      .on('transactionHash', txHash => {
+        this.transactions.push(txHash);
+      });
+  }
+
+  onTransferToken() {
+    const to = this.$refs.transferTo as HTMLInputElement;
+    const value = this.$refs.transferValue as HTMLInputElement;
+    token.methods.buyTransfer(to.value, value.value).send({
+      from: address,
+      gas: gasLimit,
+      gasPrice: gasPrice,
+    });
+  }
+
   // HTML要素を配置し終わったら呼ばれる
-  mounted() {}
+  mounted() {
+    // Metamaskの古い0.20から自分で用意した1.0.0に変換している.
+
+    web3 = new Web3(window.web3.currentProvider);
+    web3.eth.getAccounts().then(result => {
+      address = result[0];
+
+      token = new web3.eth.Contract(
+        contractInfo.Token.abi,
+        contractInfo.Token.address
+      );
+      token.methods
+        .symbol()
+        .call({ from: address }) //水色のボタン
+        .then(result => {
+          this.symbol = result;
+        });
+      token.methods
+        .totalSupply()
+        .call({ from: address }) //水色のボタン
+        .then(result => {
+          this.totalSupply = result;
+        });
+      token.methods
+        .balanceOf(address)
+        .call({ from: address }) //水色のボタン
+        .then(result => {
+          this.balance = result;
+        });
+    });
+  }
 }
 </script>
 
--- src/contractInfo.ts
+++ src/contractInfo.ts
@@ -1,8 +1,159 @@
 export default {
   Token: {
     // Every time you deploy a contract, you have to update address.
-    address: '',
+    // ブロックチェーンのアドレス
+    address: '0x1e3f93003c8a3b8eddf726fcc4ba7cf766935f76',
     // Every time you change the contract interface, you have to update abi.
-    abi: [],
+    abi: [
+      {
+        constant: true,
+        inputs: [],
+        name: 'name',
+        outputs: [
+          {
+            name: '',
+            type: 'string',
+          },
+        ],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: 'totalSupply',
+        outputs: [
+          {
+            name: '',
+            type: 'uint256',
+          },
+        ],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: 'decimals',
+        outputs: [
+          {
+            name: '',
+            type: 'uint8',
+          },
+        ],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: 'ethBalance',
+        outputs: [
+          {
+            name: '',
+            type: 'uint256',
+          },
+        ],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+      {
+        constant: true,
+        inputs: [
+          {
+            name: 'owner',
+            type: 'address',
+          },
+        ],
+        name: 'balanceOf',
+        outputs: [
+          {
+            name: '',
+            type: 'uint256',
+          },
+        ],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: 'symbol',
+        outputs: [
+          {
+            name: '',
+            type: 'string',
+          },
+        ],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+      {
+        constant: false,
+        inputs: [],
+        name: 'buyToken',
+        outputs: [],
+        payable: true,
+        stateMutability: 'payable',
+        type: 'function',
+      },
+      {
+        constant: false,
+        inputs: [
+          {
+            name: 'to',
+            type: 'address',
+          },
+          {
+            name: 'value',
+            type: 'uint256',
+          },
+        ],
+        name: 'transfer',
+        outputs: [
+          {
+            name: '',
+            type: 'bool',
+          },
+        ],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'constructor',
+      },
+      {
+        anonymous: false,
+        inputs: [
+          {
+            indexed: true,
+            name: 'from',
+            type: 'address',
+          },
+          {
+            indexed: true,
+            name: 'to',
+            type: 'address',
+          },
+          {
+            indexed: false,
+            name: 'value',
+            type: 'uint256',
+          },
+        ],
+        name: 'Transfer',
+        type: 'event',
+      },
+    ],
   },
 };
